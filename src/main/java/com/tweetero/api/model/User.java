186397cae3f898c716d425b189c0bc6e356eb061
Ivan <ivanaires32@gmail.com>
2023-11-23 18:28:31 -0300

    feat: create user

--- src/main/java/com/tweetero/api/model/User.java
+++ src/main/java/com/tweetero/api/model/User.java
@@ -7,10 +7,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "Users")
 public class User {
 
     public User(UserDTO data) {
@@ -22,9 +24,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(length = 245, nullable = false)
+    @Column(length = 50, nullable = false)
     private String username;
 
-    @Column(length = 245, nullable = false)
+    @Column(length = 50, nullable = false)
     private String avatar;
 }
